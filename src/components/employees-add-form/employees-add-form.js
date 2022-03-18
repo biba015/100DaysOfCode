975d540123e3e5ce70a938c0cfc3d8aabee24ec1
Beibars <damskali97@gmail.com>
2022-03-18 10:37:02 +0600

    done

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,40 +1,52 @@
 import { Component } from 'react';
+
 import './employees-add-form.css';
 
-
 class EmployeesAddForm extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             name: '',
-            salary: 0
+            salary: ''
         }
     }
-    onValueChange = (e) =>{
+
+    onValueChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name] : e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        // Можно еще и сообщения добавлять, подсветку, атрибуты minlength и тд.
+        if (this.state.name.length < 3 || !this.state.salary) return;
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
         })
     }
 
     render() {
-        const{name,salary} = this.state;
-         
+        const {name, salary} = this.state;
+
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit = {this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
-                        name='name'
+                        name="name"
                         value={name} 
                         onChange={this.onValueChange}/>
                     <input type="number"
                         className="form-control new-post-label"
                         placeholder="З/П в $?"
-                        name='salary'
+                        name="salary"
                         value={salary} 
                         onChange={this.onValueChange}/>
     
